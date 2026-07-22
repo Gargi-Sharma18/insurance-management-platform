6356b04ea2ae5f2bd05d1260222f8592e9942a95
Gargii-sharma <2005gargisharma18@gmail.com>
2026-07-22 19:44:14 +0530

    Intinal project set up

---
+++ src/main/java/com/insurance/backendinsuranceplateform/BackendInsuranceManagmentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.backendinsuranceplateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInsuranceManagmentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInsuranceManagmentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insurance/backendinsuranceplateform/BackendInsuranceManagmentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.backendinsuranceplateform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInsuranceManagmentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
